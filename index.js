214c0ff43bd9802eee07a48e8c79cacf9a8b4fa5
devramirez <aramirezdev1@gmail.com>
2023-06-28 18:03:28 -0400

    created init function for inquirer

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function createLogo(response) {
     const svg = setShape(response);
     fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
 }
+
+// initialize, ask questions then createLogo using responses and catch any errors
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) =>{
+        createLogo(response);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+init()
