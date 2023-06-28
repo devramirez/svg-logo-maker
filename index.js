92d5d8e3ff0711fb3f735889e1fb16f80be4dbbe
devramirez <aramirezdev1@gmail.com>
2023-06-28 17:51:54 -0400

    function for inquirer and fs

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 
+// function to create new svg file using inquirer and fs
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
+}
