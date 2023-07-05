91e6cd83dc0de1b66c864168e748091ec50db8c4
devramirez <aramirezdev1@gmail.com>
2023-07-05 17:28:02 -0400

    rewrote shapes constructor funcs

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-// require modules 
+// import modules required
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = require('./lib/questions');
-const fileName = require('./examples/logo.svg');
-const setShape = require('./lib/setShape');
+const setShape = require('./lib/shapes.test.js');
+
+// import shape classes 
+const {Triangle, Square, Circle } = require('./lib/shapes.js')
+
+
+
+
+
 
 // function to create new svg file using inquirer and fs
-function createLogo(response) {
-    const svg = setShape(response);
-    fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
-}
+// function createLogo(response) {
+//     const svg = setShape(response);
+//     fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
+// }
 
 // initialize, ask questions then createLogo using responses and catch any errors with callback 
-function init() {
-    inquirer
-    .prompt(questions)
-    .then((response) =>{
-        createLogo(response);
-    })
-    .catch(err => {
-        console.log(err)
-    });
-}
+// function init() {
+//     inquirer
+//     .prompt(questions)
+//     .then((response) => {
+//         createLogo(response);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     });
+// }
 
-init()
+// init();
