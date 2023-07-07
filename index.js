41509bb5235e68a3cd5e974bce856fd61867a978
devramirez <aramirezdev1@gmail.com>
2023-07-06 20:31:29 -0400

    writing conditional statements for all shapes

--- index.js
+++ index.js
@@ -6,7 +6,33 @@ const setShape = require('./lib/shapes.test.js');
 // import shape classes 
 const {Triangle, Square, Circle } = require('./lib/shapes.js')
 
+// function writes the SVG file using inquirer to prompt user 
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
+  svgString += "<g>";
+  // Takes user input for shape choice and inserts it into SVG file
+  svgString += `${answers.shape}`;
 
+  // conditional statment checks user input from color array 
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  
+
+
+}
 
 
 
